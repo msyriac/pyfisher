ff978dae46a840126590abf09b0d43927eba8257
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-11-19 23:37:52 -0500

    bao and cmb progress

---
+++ bin/save_bao.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+from orphics import maps,io,cosmology,stats,mpi
+import numpy as np
+import os,sys
+import pyfisher
+
+import argparse
+# Parse command line
+parser = argparse.ArgumentParser(description='Do a thing.')
+parser.add_argument("exp_name", type=str,help='Positional arg.')
+parser.add_argument("--boss-include",     type=str,  default='6df,mgs,lowz,cmass',help="A description.")
+parser.add_argument("--input-path",     type=str,  default='input',help="Relative path to directory with experiment info.")
+required_args = parser.add_argument_group('Required arguments')
+required_args.add_argument("-o","--output",type=str,help="Output root",required=True)
+required_args.add_argument("-p","--param-file",type=str,help="Parameter file",required=True)
+args = parser.parse_args()
+
+
+zs,sig_pers = pyfisher.load_bao_experiment_rs_dV_diagonal(args.exp_name,args.input_path,boss_include=args.boss_include.split(','))
+param_dat = np.genfromtxt(args.param_file,dtype=None,encoding='utf-8',delimiter=',')
+jobs = []
+jobs.append((None,None,'f'))
+fids = {}
+for p in param_dat:
+    param = p[0]
+    fid = p[1]
+    fids[param] = fid
+    pstr = str(p[2]).strip()
+    if pstr[-1]=='%': 
+        step = float(pstr[:-1])*np.abs(fid)/100.
+    else:
+        step = float(pstr)
+    print(step)
+    assert step>0
+    jobs.append((param,fid+step,'u'))
+    jobs.append((param,fid-step,'d'))
+
+njobs = len(jobs)
+comm,rank,my_tasks = mpi.distribute(njobs)
+
+
+
+for task in my_tasks:
+    param,val,ptype = jobs[task]
+    print(param,val,ptype)
+    pparams = dict(fids)
+    if param is None:
+        assert val is None
+        assert ptype=='f'
+    else:
+        pparams[param] = val
+
+    retval = pyfisher.get_bao_rs_dV(zs,params=pparams,engine='camb',de='ppf')
+
+    if param is None:
+        fname = f'{args.output}_bao_fiducial.txt'
+    else:
+        fname = f'{args.output}_bao_{param}_{ptype}.txt'
+    hstr = ','.join([param,str(val)]) if param is not None else ""
+    np.savetxt(fname,retval,header=hstr)
+
+if rank==0:
+
+    def read(param,ud):
+        filename = f'{args.output}_bao_{param}_{ud}.txt'
+        with open(filename,'r') as f:
+            header = f.readline().strip()
+            assert header[0]=="#"
+            oparam,val = header[1:].split(',')
+        data = np.loadtxt(filename)
+        return oparam.strip(),float(val),data
+
+    fiducial_theory = np.loadtxt(f'{args.output}_bao_fiducial.txt')
+    deriv_theory = {}
+    for param in fids.keys():
+        uparam,uval,udata = read(param,'u')
+        dparam,dval,ddata = read(param,'d')
+        assert param==uparam==dparam
+        deriv_theory[param] = (udata-ddata) / (uval-dval)
+    Fmat = pyfisher.get_bao_fisher_rs_dV_diagonal(list(fids.keys()),deriv_theory,fiducial_theory,sig_pers)
+    stats.write_fisher(f'{args.output}_bao_fisher.txt',Fmat,delim=',')
+    print(Fmat)
--- pyfisher/pyfisher.py
+++ pyfisher/pyfisher.py
@@ -1,10 +1,12 @@
 from __future__ import print_function
-from orphics import mpi
+from orphics import mpi,cosmology,stats
 import numpy as np
 import os,sys
 from classy import Class
 import camb
 from camb import model
+import sympy
+import warnings
 
 """
 Unified framework for supporting:
@@ -50,14 +52,65 @@ Unified framework for supporting:
 #             fname = output_root + f"_{obs}_{param}_deriv.txt"
 #             d = (forward[obs] - backward[obs])/2./step
 
-        
+
+def _camb_to_class(params):
+    if params['thetastar'] is not None:
+        params['100*theta_s'] = params.pop('thetastar')*100.
+    else:
+        params.pop('thetastar')
+    if params['ctheta'] is not None:
+        params['100*theta_s'] = params.pop('ctheta')*100.
+        warnings.warn('Replacing CLASS 100*theta_s with cosmomc_theta')
+    else:
+        params.pop('ctheta')
+    if params['H0'] is None:
+        params.pop('H0')
+    params['A_s'] = params.pop('As')
+    params['n_s'] = params.pop('ns')
+    params['Omega_cdm'] = params.pop('omch2')
+    params['omega_b'] = params.pop('ombh2')
+    params['tau_reio'] = params.pop('tau')
+    params['Omega_k'] = params.pop('ok')
+    params['N_ur'] = params.pop('nnu')
+    #params['m_ncdm'] = ','.join([str(params.pop('mnu')/3.)]*3)
+    params['N_ncdm'] = 1
+    params['m_ncdm'] = params.pop('mnu')
+    
+    # params['use_ppf'] = 'no'
+    # params['fluid_equation_of_state'] = 'CLP'
+    # params['w0_fld'] = params.pop('w0')
+    # params['wa_fld'] = params.pop('wa')
+    # params['cs2_fld'] = params.pop('cs2')
+
+    # params.pop('mnu')
+    # DARK ENERGY AND R NOT SUPPORTED
+    params.pop('w0')
+    params.pop('wa')
+    params.pop('cs2')
+    params.pop('r')
+    return params
+
 
 def map_params(params,engine='camb'):
-    h = params['H0'] / 100.
-    if 'omm' in params.keys():
-        assert 'omch2' not in params.keys()
-        
-
+    checksum = 0
+    try:
+        assert params['H0']
+        checksum = checksum + 1
+    except:
+        pass
+    try:
+        assert params['thetstar']
+        checksum = checksum + 1
+    except:
+        pass
+    try:
+        assert params['ctheta']
+        checksum = checksum + 1
+    except:
+        pass
+    assert checksum <= 1
+    if params is None: params = dict({})
+    params = set_defaults(params)
     if engine=='camb':
         return params
     elif engine=='class':
@@ -89,7 +142,7 @@ def set_defaults(params):
     
     
 
-def set_camb_pars(params=None,de='fluid'):
+def set_camb_pars(params=None,de='ppf'):
     """
     We only allow an As,ns,omch2,ombh2,H0,tau,mnu,w0,wa,omk,r,ctheta,nnu,
     cs2,thetastar parametrization. (ctheta=cosmomc_theta)
@@ -104,6 +157,7 @@ def set_camb_pars(params=None,de='fluid'):
                        omk=params['ok'], tau=params['tau'],nnu=params['nnu'],
                        cosmomc_theta=params['ctheta'],thetastar=params['thetastar'])
     pars.InitPower.set_params(As=params['As'], ns=params['ns'], r=params['r'])
+    if params['r']>0: pars.WantTensors = True
     pars.set_dark_energy(w=params['w0'], wa=params['wa'], cs2=params['cs2'],dark_energy_model=de)
     return pars
 
@@ -127,15 +181,15 @@ def get_s8(zs=[0.],params=None,nonlinear=False,kmax=5.2,**kwargs):
     s8 = np.array(results.get_sigma8())
     return s8[::-1] if rev else s8
 
-def get_bao(params,zs,engine='camb'):
+def get_bao_rs_dV(zs,params=None,engine='camb',de='ppf'):
+    #FIXME: camb and class only agree at 3% level!!!
     params = map_params(params,engine=engine)
     if engine=='camb':
-        pars = camb.CAMBparams()
-        #This function sets up CosmoMC-like settings, with one massive neutrino and helium set using BBN consistency
-        pars.set_cosmology(H0=67.5, ombh2=0.022, omch2=0.122, mnu=0.06, omk=0, tau=0.06)
-        pars.InitPower.set_params(As=2e-9, ns=0.965, r=0)
-        pars.set_for_lmax(2500, lens_potential_accuracy=0)
+        pars = set_camb_pars(params=params,de=de)
+        results = camb.get_results(pars)
+        retval = results.get_BAO(zs,pars)[:,0]
     elif engine=='class':
+        zs = np.asarray(zs)
         cosmo = Class()
         params['output'] = ''
         cosmo.set(params)
@@ -149,10 +203,52 @@ def get_bao(params,zs,engine='camb'):
     return retval
 
 
-def get_cls(params,lmax=3000,engine='camb'):
+def get_bao_fisher_rs_dV_diagonal(param_list,deriv_theory,fiducial_theory,sigma_percents):
+    nparams = len(param_list)
+    Fisher = np.zeros((nparams,nparams))
+    sigmas = sigma_percents*fiducial_theory/100.
+    for i in range(nparams):
+        for j in range(i,nparams):
+            param1 = param_list[i]
+            param2 = param_list[j]
+            rsdV1 = deriv_theory[param1]
+            rsdV2 = deriv_theory[param2]
+            Fz = (rsdV1*rsdV2/sigmas**2.).sum()
+            Fisher[i,j] = Fz
+            if i!=j: Fisher[j,i] = Fz
+    return stats.FisherMatrix(Fisher,param_list)
+
+def load_bao_experiment_rs_dV_diagonal(exp_name,data_path,boss_include=['6df','mgs','lowz','cmass']):
+    if exp_name=='desi':
+        zs,sig_pers = np.loadtxt(f'{data_path}/desi.txt',unpack=True)
+    elif exp_name=='boss':
+        array = np.genfromtxt(f'{data_path}/boss.txt',delimiter=',',dtype=None,encoding='utf-8')
+        zs = []
+        sig_pers = []
+        for line in array:
+            if line[0]  in boss_include: 
+                zs.append(line[1])
+                sig_pers.append(line[2])
+    else:
+        raise ValueError
+    return np.asarray(zs),np.asarray(sig_pers)
+
+def get_cls(params=None,lmax=3000,accurate=False,engine='camb',de='ppf',nonlinear=True):
     params = map_params(params,engine=engine)
     if engine=='camb':
-        pass
+        pars = set_camb_pars(params=params,de=de)
+        if accurate:
+            pars.set_accuracy(AccuracyBoost=3.0, lSampleBoost=1.0, lAccuracyBoost=3.0)
+            pars.set_for_lmax(lmax=int(lmax+500), lens_potential_accuracy=3.0, max_eta_k=20000)
+        else:
+            pars.set_accuracy(AccuracyBoost=1.0, lSampleBoost=1.0, lAccuracyBoost=1.0)
+            pars.set_for_lmax(lmax=int(lmax+500), lens_potential_accuracy=1, max_eta_k=2*lmax)
+        if nonlinear:
+            pars.NonLinear = model.NonLinear_both
+        else:
+            pars.NonLinear = model.NonLinear_none
+        return load_theory(pars,lpad=lmax+2)
+        
     elif engine=='class':
         cosmo = Class()
         params['output'] = 'lCl,tCl,pCl'
@@ -165,7 +261,38 @@ def get_cls(params,lmax=3000,engine='camb'):
 
 
 def get_trans_deriv(iparam,oparam,fiducials,deriv_path):
-    pass
+    """
+    The matrix is input/output
+
+    So it has elements like
+
+    dAs/ds8, d(omc*h**2)/dom, d(omc*h**2)/ds8
+    """
+
+    if iparam==oparam: 
+        return 1
+
+    fs = fiducials
+    h = fs['H0']/100.
+    oc = fs['omch2'] / h**2.
+    ob = fs['ombh2'] / h**2.
+    if iparam=='omch2' and oparam=='om':
+        return h**2.
+    elif iparam=='ombh2' and oparam=='om':
+        return h**2.
+    elif iparam=='omch2' and oparam=='H0':
+        return 2. * h * oc / 100.
+    elif iparam=='ombh2' and oparam=='H0':
+        return 2. * h * ob / 100.
+    elif oparam=='s8':
+        if iparam in ['As','ns','omch2','ombh2','ok','mnu','w0','wa','nnu','ctheta','thetastar','cs2']:
+            val = np.loadtxt(derivpath+f"_fitderiv_s8_wrt_{iparam}.txt")
+            assert val.size==1
+            return 1./val.ravel()[0]
+        elif iparam in ['tau','r']:
+            return 0.
+        else:
+            raise ValueError
     
 
 def reparameterize(Fmat,iparams,oparams,fiducials,deriv_path):
@@ -175,12 +302,7 @@ def reparameterize(Fmat,iparams,oparams,fiducials,deriv_path):
     like As,ns,omch2,ombh2,H0,tau,mnu,w0,wa,omk,r.
     oparams can contain those as well as
     s8
-    om_m
-    om_b
-    Symbol
-
-    where Symbol can be a sympy.Symbol expression,
-    e.g. Symbol(s8) * (Symbol(om_m)/0.3)**0.25
+    om
     """
 
     onum = len(oparams)
@@ -191,9 +313,42 @@ def reparameterize(Fmat,iparams,oparams,fiducials,deriv_path):
     for i in range(inum):
         for j in range(onum):
             M[i,j] = get_trans_deriv(iparams[i],oparams[j],fiducials,deriv_path)
-
+    print(M)
     return np.dot(np.dot(M,Fmat),M.T)
 
 
 #def s8(params):
     
+
+def load_theory(pars,lpad=9000):
+    '''
+    All ell and 2pi factors are stripped off.
+    '''
+
+    uSuffix = "unlensed_total"
+    lSuffix = "total"
+
+    results = camb.get_results(pars)
+    cmbmat = results.get_cmb_power_spectra(pars,lmax=lpad,spectra=['total','unlensed_total','lens_potential'],raw_cl=True,CMB_unit='muK')
+
+    theory = cosmology.TheorySpectra()
+    for i,pol in enumerate(['TT','EE','BB','TE']):
+        cls =cmbmat[lSuffix][2:,i]
+        ells = np.arange(2,len(cls)+2,1)
+        theory.loadCls(ells,cls,pol,lensed=True,interporder="linear",lpad=lpad,fill_zero=True)
+        cls = cmbmat[uSuffix][2:,i]
+        ells = np.arange(2,len(cls)+2,1)
+        theory.loadCls(ells,cls,pol,lensed=False,interporder="linear",lpad=lpad,fill_zero=True)
+
+    lensArr = cmbmat['lens_potential']
+    cldd = lensArr[2:,0]
+    ells = np.arange(2,len(cldd)+2,1)
+    clkk = cldd / 4. * (ells*(ells+1))**2.
+    theory.loadGenericCls(ells,clkk,"kk",lpad=lpad,fill_zero=True)
+    theory.dimensionless = False
+    return theory
+
+
+
+
+
