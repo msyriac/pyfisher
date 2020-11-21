ea4917ebfb52b22bd761d212b4bfbf7add2d3c4a
Mathew Madhavacheril <mathewsyriac@gmail.com>
2020-11-20 23:03:33 -0500

    fix output path

--- bin/save_cmb_fisher.py
+++ bin/save_cmb_fisher.py
@@ -13,6 +13,8 @@ required_args.add_argument("-p","--param-file",type=str,help="Parameter file",re
 required_args.add_argument("-o","--output",type=str,help="Output root",required=True)
 args = parser.parse_args()
 
+out_name = pyfisher.prepare_output(args,"save_cmb_fisher.py Planck CMB Fishers run")
+
 param_dat = np.genfromtxt(args.param_file,dtype=None,encoding='utf-8',delimiter=',')
 _,fids = pyfisher.get_param_info(args.param_file,exclude=None)
 
@@ -20,5 +22,5 @@ param_list = list(fids.keys())
 F1 = pyfisher.get_planck_cmb_fisher(param_list,np.arange(2,31),['TT'],args.input,fsky=1)
 F2 = pyfisher.get_planck_cmb_fisher(param_list,np.arange(30,2501),['TT','EE','TE'],args.input,fsky=1)
 
-pyfisher.write_fisher(f'{args.output}_planck_low_ell_TT_fullsky.txt',F1,delim=',')
-pyfisher.write_fisher(f'{args.output}_planck_high_ell_TTEETE_fullsky.txt',F2,delim=',')
+pyfisher.write_fisher(f'{out_name}_planck_low_ell_TT_fullsky.txt',F1,delim=',')
+pyfisher.write_fisher(f'{out_name}_planck_high_ell_TTEETE_fullsky.txt',F2,delim=',')
